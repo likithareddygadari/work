24b49ca94ca547a762c27e78b8261d1c74c2073c
likithareddy <likitha00765@gmail.com>
2018-10-04 18:33:41 -0400

    minor change

--- triangle/src/triangle/triangle.java
+++ triangle/src/triangle/triangle.java
@@ -9,14 +9,15 @@ public class triangle {
 		double a;
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Input s1: ");
-		s1 = sc.nextInt();
+		s1 = sc.nextDouble();
 		System.out.println("Input s2: ");
-		s2 = sc.nextInt();
+		s2 = sc.nextDouble();
 		System.out.println("Input s3: ");
-		s3 = sc.nextInt();
+		s3 = sc.nextDouble();
 		
 		s= (s1+s2+s3) / 2;
 		
+		System.out.println(s);
 		a= Math.sqrt(s * (s-s1) * (s-s2) * (s-s3));
 		System.out.println(a);
 		
