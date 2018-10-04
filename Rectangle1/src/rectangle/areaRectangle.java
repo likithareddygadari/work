8ef5333d92a6a388cd2c3819bd00d70e9d45d911
likithareddy <likitha00765@gmail.com>
2018-10-04 14:26:58 -0400

    rectangle

---
+++ Rectangle1/src/rectangle/areaRectangle.java
@@ -0,0 +1,19 @@
+package rectangle;
+
+import java.util.Scanner;
+
+public class areaRectangle {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a*b;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
---
+++ Rectangle1/src/rectangle/perimeterRectangle.java
@@ -0,0 +1,18 @@
+package rectangle;
+
+import java.util.Scanner;
+
+public class perimeterRectangle {
+
+	public static void main(String[] args) {
+		int x,y,z;
+		Scanner sc = new Scanner(System.in);
+		x= sc.nextInt();
+		y=sc.nextInt();
+		z= 2*(x+y);
+		System.out.println(z);
+		
+
+	}
+
+}
